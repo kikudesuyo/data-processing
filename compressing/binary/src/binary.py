e2df13f0063aa4e5d28528a034a120cffba1cecf
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2022-09-30 15:50:33 +0900

    fixed

--- compressing/binary/src/binary.py
+++ compressing/binary/src/binary.py
@@ -1,14 +1,18 @@
 # 作成させたfilenameから文字列を作成
 for i in range(4):
-    f = open('../../../image-text-convertion//temp/part'+str(i+1)+'.txt','r')
+    f = open('../../../image-text-convertion/temp/part'+str(i+1)+'.txt','r')
     body = f.read()
     f.close()
 # 文字列をバイナリ表示
 binary_converted = ' '.join(format(ord(c), 'b') for c in body)
+print(len(binary_converted))
 # バイナリ表示させた変数をテキストファイルに出力
-binary_textfile = '../temp/binary_textfile' + '.bin'
-f = open(binary_textfile, 'w')
-f.write(binary_converted)
+for i in binary_converted:
+    if len(binary_converted) % 25 == 0:
+        binary_textfile = '../temp/binary_textfile' +str(i) +'.txt'
+        f = open(binary_textfile, 'w')
+        f.write(binary_converted)
+
 
 # f = open("../temp/myfile_binaryfile.dat", "rb")
 # f.write(binary_textfile, "wb")
